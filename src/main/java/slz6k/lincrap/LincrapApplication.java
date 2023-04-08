94117e0508f175eb4e6e231014cd452a8e231712
민경 <tjalsrud0101@gmail.com>
2023-04-08 13:03:20 +0900

    Initial commit

---
+++ src/main/java/slz6k/lincrap/LincrapApplication.java
@@ -0,0 +1,13 @@
+package slz6k.lincrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LincrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LincrapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/slz6k/lincrap/LincrapApplicationTests.java
@@ -0,0 +1,13 @@
+package slz6k.lincrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LincrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
